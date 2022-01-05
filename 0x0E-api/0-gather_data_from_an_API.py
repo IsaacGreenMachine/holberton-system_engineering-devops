99d790c6c8f56270f7666fd62dc776d142a6d26f
Isaac <zuderg@gmail.com>
2022-01-05 00:25:45 -0600

    added script

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python3
+import json
+import sys
+from urllib import request
 """returns information about employee progress"""
+
+
+def getData(empID):
+    """function to get info from html request"""
+    tdUrl = "https://jsonplaceholder.typicode.com/users/{}/todos".format(empID)
+    userUrl = "https://jsonplaceholder.typicode.com/users/{}".format(empID)
+    with request.urlopen(tdUrl) as todoResponse:
+        with request.urlopen(userUrl) as response:
+            loadedTodo = json.loads(todoResponse.read())
+            loadedResponse = json.loads(response.read())
+            employeeName = loadedResponse.get("name")
+            numTasks = len(loadedTodo)
+            doneTasks = sum(map(lambda x: x.get("completed"), loadedTodo))
+            print("Employee {} is done with tasks({}/{}):".format(
+                employeeName,
+                doneTasks,
+                numTasks))
+            for task in loadedTodo:
+                if task.get("completed"):
+                    print("\t {}".format(task.get("title")))
+
+
+if __name__ == "__main__":
+    getData(sys.argv[1])
